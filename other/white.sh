d51cdf6c9ace96dd2711c0a20a03fc053b1e0af3
EdwinRodger <vaidiktest@gmail.com>
2022-04-19 01:06:27 +0530

    Added red icon svg

--- other/white.sh
+++ other/white.sh
@@ -3,14 +3,14 @@
 
 white="stroke\s*:\s*\(#fff\|#ffffff\|white\)"
 
-replace="stroke:#fff"
+replace="stroke:#ff0000"
 for x in *.svg; do
 	sed -i "s/${white}/${replace}/Ig" "${x}"
 done
 
 white="fill\s*:\s*\(#fff\|#ffffff\|white\)"
 
-replace="fill:#fff"
+replace="fill:#ff0000"
 for x in *.svg; do
 	sed -i "s/${white}/${replace}/Ig" "${x}"
 done
